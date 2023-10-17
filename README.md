38a6aeaa866c36d6306d2b79d9ffeb88b48ca33a
Brent Marquez <brentonmarquez@gmail.com>
2023-10-17 12:43:34 -0600

    make call to backend service to get access token to get around CORS

--- README.md
+++ README.md
@@ -4,4 +4,12 @@
 - https://www.feathery.io/blog/linkedin-oauth
 
 ### Library used:
+
 - https://www.npmjs.com/package/react-linkedin-login-oauth2
+
+### CORS
+
+- getting cors error requesting access token:
+  https://stackoverflow.com/questions/69254424/cors-error-on-linkedin-oauth-v2-accesstoken-api-from-frontend
+
+- Need to get access token from a server, not a browser to get around this.
--- src/LoginWithLinkedIn.tsx
+++ src/LoginWithLinkedIn.tsx
@@ -5,7 +5,6 @@ import linkedin from "react-linkedin-login-oauth2/assets/linkedin.png";
 function LoginWithLinkedIn() {
   const REDIRECT_URL = `${window.location.origin}/login`;
   const CLIENT_ID = import.meta.env.VITE_LINKEDIN_CLIENT_ID;
-  const CLIENT_SECRET = import.meta.env.VITE_LINKEDIN_SECRET;
 
   const { linkedInLogin } = useLinkedIn({
     clientId: CLIENT_ID,
@@ -13,33 +12,14 @@ function LoginWithLinkedIn() {
     onSuccess: (code) => {
       console.log({ code });
 
-      // Now that we have the code, need to use it to log in with Firebase somehow
-      // get user info to store in firebase
-      const LINKEDIN_URL = "https://www.linkedin.com/oauth/v2/accessToken";
-
-      const params = {
-        grant_type: "authorization_code",
-        code: code,
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        redirect_uri: REDIRECT_URL,
-      };
-      // not working - get 400 bad request
+      // call backend to get access token
       axios
-        .post(
-          LINKEDIN_URL,
-          {},
-          {
-            params,
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          }
-        )
-        .then((res) => {
-          console.log({ res });
-        })
-        .catch((e) => {
-          console.error("error calling linedin to get accesstoken", e);
-        });
+        .get("http://localhost:5000/linkedin", { params: { code } })
+        .then((res) => console.log({ res }))
+        .catch((e) => console.error(e));
+
+      // Now that we have the code, need to use it to log in with Firebase somehow
+      // get user info to store in firebase and sign in with custom token?
     },
     onError: (error) => {
       // NOTE: error will come up for user closed pop up - probably can ignore this as popup should close after signin.
