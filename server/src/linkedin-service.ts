8bcfbeda0ea8a5fb844ef046764f3556287a9f7a
Brent Marquez <brentonmarquez@gmail.com>
2023-10-17 14:23:03 -0600

    refactor add linkedin service to use async await

---
+++ server/src/linkedin-service.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const LINKEDIN_BASE_URL = "https://api.linkedin.com/v2";
+const authorizedEndpoint = "userinfo";
+
+const REDIRECT_URL = process.env.REDIRECT_URI;
+const CLIENT_ID = process.env.LINKEDIN_CLIENT_ID;
+const CLIENT_SECRET = process.env.LINKEDIN_SECRET;
+const LINKEDIN_URL = "https://www.linkedin.com/oauth/v2/accessToken";
+
+/**
+ * { token_type, access_token, id_token, expires_in } = token response;
+ * */
+export const getAccessToken = async (code) => {
+  const params = {
+    grant_type: "authorization_code",
+    code: code,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    redirect_uri: REDIRECT_URL,
+  };
+
+  const res = await axios.post(
+    LINKEDIN_URL,
+    {},
+    {
+      params,
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    }
+  );
+  return res.data;
+};
+
+export const getLinkedinUser = async ({ token_type, access_token }) => {
+  const res = await axios.get(`${LINKEDIN_BASE_URL}/${authorizedEndpoint}`, {
+    headers: { Authorization: `${token_type} ${access_token}` },
+  });
+
+  const {
+    // email_verified,
+    // name,
+    // locale,
+    sub,
+    given_name,
+    family_name,
+    email,
+    picture,
+  } = res.data;
+
+  return { sub, email, given_name, family_name, picture };
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,82 +1,32 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import axios from "axios";
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+import { getAccessToken, getLinkedinUser } from "./linkedin-service";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const REDIRECT_URL = process.env.REDIRECT_URI;
-const CLIENT_ID = process.env.LINKEDIN_CLIENT_ID;
-const CLIENT_SECRET = process.env.LINKEDIN_SECRET;
-const LINKEDIN_URL = "https://www.linkedin.com/oauth/v2/accessToken";
+app.get("/linkedin", async (req, res) => {
+  try {
+    const code = req.query.code;
 
-app.get("/linkedin", (req, res) => {
-  const code = req.query.code;
+    if (!code) {
+      return res.json({ status: 400, message: "Missing code." });
+    }
 
-  if (!code) {
-    return res.json({ status: 400, message: "Missing code." });
+    const { token_type, access_token } = await getAccessToken(code);
+
+    const linkedInUser = await getLinkedinUser({ token_type, access_token });
+
+    console.log({ linkedInUser });
+    res.status(200).json({ message: "Success" });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Error /linkedin" });
   }
-
-  const params = {
-    grant_type: "authorization_code",
-    code: code,
-    client_id: CLIENT_ID,
-    client_secret: CLIENT_SECRET,
-    redirect_uri: REDIRECT_URL,
-  };
-
-  axios
-    .post(
-      LINKEDIN_URL,
-      {},
-      {
-        params,
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      }
-    )
-    .then((response) => {
-      const data = response.data;
-      const { token_type, access_token, id_token, expires_in } = data;
-      console.log({ token_type, access_token, id_token, expires_in });
-
-      const LINKEDIN_BASE_URL = "https://api.linkedin.com/v2";
-      const authorizedEndpoint = "userinfo";
-
-      axios
-        .get(`${LINKEDIN_BASE_URL}/${authorizedEndpoint}`, {
-          headers: { Authorization: `${token_type} ${access_token}` },
-        })
-        .then((linkedinData) => {
-          const {
-            // email_verified,
-            // name,
-            // locale,
-            sub,
-            given_name,
-            family_name,
-            email,
-            picture,
-          } = linkedinData.data;
-          // can use these to create and store a user in firebase
-
-          // custom token - service account?
-          console.log({ sub, email, given_name, family_name, picture });
-        })
-        .catch((e) => {
-          console.error("Error getting me profile: ", e);
-        });
-
-      res.json("Success - access token recieved");
-    })
-    .catch((e) => {
-      console.error("error calling linedin to get accesstoken", e);
-      res.status(500).json({ error: "There was an error get access token" });
-    });
 });
 
 const PORT = process.env.PORT || 5000;
