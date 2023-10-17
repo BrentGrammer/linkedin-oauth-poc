8a1c2d8bbae89c82dd056a4e7b11bd806c61875e
Brent Marquez <brentonmarquez@gmail.com>
2023-10-17 11:16:49 -0600

    basic popup signin with linkedin - also installed firebase and linkedin oauth package

---
+++ src/LoginWithLinkedIn.tsx
@@ -0,0 +1,29 @@
+import { useLinkedIn } from "react-linkedin-login-oauth2";
+import linkedin from "react-linkedin-login-oauth2/assets/linkedin.png";
+
+function LoginWithLinkedIn() {
+  const REDIRECT_URL = encodeURIComponent("http://localhost:5173");
+
+  const { linkedInLogin } = useLinkedIn({
+    clientId: import.meta.env.VITE_LINKEDIN_CLIENT_ID,
+    redirectUri: REDIRECT_URL,
+    onSuccess: (code) => {
+      console.log(code);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+    scope: "email openid profile",
+  });
+
+  return (
+    <img
+      src={linkedin}
+      alt="Log in with Linked In"
+      style={{ maxWidth: "180px" }}
+      onClick={linkedInLogin}
+    />
+  );
+}
+
+export default LoginWithLinkedIn;
---
+++ src/config/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "authtest-3e4f7.firebaseapp.com",
+  projectId: "authtest-3e4f7",
+  storageBucket: "authtest-3e4f7.appspot.com",
+  messagingSenderId: "369943110242",
+  appId: "1:369943110242:web:2bf4335dace321ab62273b",
+};
+
+export const app = initializeApp(firebaseConfig);
