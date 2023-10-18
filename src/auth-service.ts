b1201ae18f90cd206892ea8f9fa8388974f4aba4
Brent Marquez <brentonmarquez@gmail.com>
2023-10-18 11:24:43 -0600

    rename var

--- src/auth-service.ts
+++ src/auth-service.ts
@@ -3,12 +3,12 @@ import { getAuth, signInWithCustomToken } from "firebase/auth";
 
 /**
  * calls our backend server which uses firebase-admin to generate and return a custom firebase token to sign the user in with firebase auth.
- * @param code code received from Linkedin after initiating oauth signin
+ * @param codeFromLinkedin code received from Linkedin after initiating oauth signin
  * @returns custom firebase token created on the backend using firebase-admin
  */
-export const getSigninToken = async (code: string): Promise<string> => {
+export const getSigninToken = async (codeFromLinkedin: string): Promise<string> => {
   const res = await axios.post("http://localhost:5000/linkedin", {
-    code,
+    code: codeFromLinkedin,
   });
   return res.data;
 };
